414d17e6fc522a84304bc7269e862305ad318b58
HenriqueFreire <henriquefreire@outlook.com.br>
2026-03-21 13:15:37 -0300

    Módulo finalizado: Conhecendo a Linguagem de Programação Python

---
+++ Fundamentos-de-Python/Conhecendo-a-Linguagem-de-Programacao-Python/convertendo_tipos.py
@@ -0,0 +1,15 @@
+print(int(1.2))
+print(int("10"))
+print(float("10.3"))
+print(float(10))
+print(str(10.10))
+
+print(type(str(5.2)))
+
+valor = 10
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+print(100 / 2)
+print(100 // 2)
---
+++ Fundamentos-de-Python/Conhecendo-a-Linguagem-de-Programacao-Python/variaveis_contantes.py
@@ -0,0 +1,12 @@
+nome = "Henrique"
+idade = 32
+
+nome, idade = "Jaqueline", 29
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "MG", "BA"]
+
+print(BRAZILIAN_STATES)
