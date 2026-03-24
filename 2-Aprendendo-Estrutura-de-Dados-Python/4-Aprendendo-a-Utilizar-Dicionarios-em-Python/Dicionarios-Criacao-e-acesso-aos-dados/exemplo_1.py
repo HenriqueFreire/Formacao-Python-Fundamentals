144bb5d5b72bded5d51b03f896ececa4a80c0453
HenriqueFreire <henriquefreire@outlook.com.br>
2026-03-24 18:52:51 -0300

    Módulo finalizado: Aprendendo a Utilizar Dicionarios em Python

---
+++ 2-Aprendendo-Estrutura-de-Dados-Python/4-Aprendendo-a-Utilizar-Dicionarios-em-Python/Dicionarios-Criacao-e-acesso-aos-dados/exemplo_1.py
@@ -0,0 +1,9 @@
+pessoa = {"nome": "Henrique", "idade": 32}
+
+pearson = dict(nome="Henrique", idade=28)
+
+pessoa["telefone"] = "3334-1223" # {"nome": "Henrique", "idade": 28, "telefone": "3334-1234"}
+
+pessoa["nome"] # "Henrique"
+pessoa["idade"] # 32
+pessoa["telefone"] # "3334-1234"
